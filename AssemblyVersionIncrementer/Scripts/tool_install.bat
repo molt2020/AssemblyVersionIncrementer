a69e7010869c20e78a16c513057b4cdb6a498351
molt2020@github <molt2020@github.com>
2024-05-08 13:51:30 +0100

    adding tags to package and script for dotnettool installation (must be run manually)

---
+++ AssemblyVersionIncrementer/Scripts/tool_install.bat
@@ -0,0 +1,5 @@
+﻿@echo off
+echo.
+echo.Installing AssemblyVersionIncrementer as dotnet tool
+echo.
+dotnet tool install --global --add-source AssemblyVersionIncrementer.nupkg  AssemblyVersionIncrementer
