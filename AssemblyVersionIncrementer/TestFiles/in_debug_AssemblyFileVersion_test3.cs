16bc7c026e011226ebb9227d384f022d567c80bb
molt2020@github <molt2020@github.com>
2024-05-08 17:12:37 +0100

    test fixtures for debug mode

---
+++ AssemblyVersionIncrementer/TestFiles/in_debug_AssemblyFileVersion_test3.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("1.23.4.5")]
+[assembly: AssemblyFileVersion("2.34.5.6")]
---
+++ Tests/ConsoleUtilTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyVersionIncrementer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyVersionIncrementer.Tests
+{
+    [TestClass()]
+    public class ConsoleUtilTests
+    {
+        [TestMethod()]
+        public void ErrorsToConsoleTest()
+        {
+            List<IncrementerError> errors = new List<IncrementerError>()
+            {
+                IncrementerError.Error("error"),
+                IncrementerError.Ok("ok"),
+                IncrementerError.Info("info"),
+                IncrementerError.Warning("warning")
+            };
+            ConsoleUtil.ErrorsToConsole(errors, "category");
+        }
+    }
+}
